91ce5dd218b004feec8f1c973d0c1e725c8dd1d5
Roman Kuraev <ololo@desude.su>
2020-11-24 16:18:51 +0300

    calculate frequency based on timestamps, show avg/min/max latency

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const [src, dstDir] = process.argv.slice(2);
 
 // logger sets FS_SEL to 3: range is ±2000°/s, sensitivity is 16.4°/s
 const GYRO_SCALE = 16.4; 
-// logging rate in readings/s
-const RATE = 500;
 
 const convertRawToRadiansPerSecond = (raw) => (raw / GYRO_SCALE) * (Math.PI / 180);
 
@@ -37,24 +35,52 @@ const mpuRawStream = new Parser()
     readUntil: "eof",
   });
 
-readFile(src, function (err, data) {
-  const pathObject = parse(src);
+console.log(`Reading raw data from ${src}`);
 
-  console.log(`Reading raw data from ${src}`);
+readFile(src, function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  const pathObject = parse(src);
 
   const parsed = mpuRawStream.parse(data);
 
   const stat = parsed.blocks.reduce((acc, block) => {
     acc.count += block.count;
     acc.overruns += block.overruns;
-    return acc;
-  }, { count: 0, overruns: 0 });
+    acc.timestamps.push(...block.readings.slice(0, block.count).map(r => r.timestamp));
 
-  console.log(`length: ${stat.count / RATE} sec`);
-  console.log(`overruns: ${stat.overruns}`);
+    return acc;
+  }, { count: 0, overruns: 0, timestamps: [] });
+
+  const latencyStat = stat.timestamps.reduce(
+    (acc, reading, index) => {
+      if (index) {
+        const frameLatency = reading - stat.timestamps[index -1];
+        acc.sum += frameLatency;
+        acc.min = acc.min ? Math.min(acc.min, frameLatency) : frameLatency;
+        acc.max = acc.max ? Math.max(acc.max, frameLatency) : frameLatency;
+      }
+
+      return acc;
+    },
+    {min: null, max: null, sum: 0}
+  );
+  
+  const avgLatency = latencyStat.sum / stat.count;
+  const frequency = 1e6 / avgLatency;
+
+  console.log(`length: ${stat.count / frequency} sec`);
+  console.log(`latency: avg ${avgLatency} us, min ${latencyStat.min} us, max ${latencyStat.max} us`);
+  console.log(`avg frequency: ${frequency} Hz`);
+
+  if (stat.overruns) {
+    console.log(`Warning! ${stat.overruns} overruns detected. Check logger configuration`);
+  }
 
   const out = {
-    frequency: RATE,
+    frequency,
     angular_velocity_rad_per_sec: parsed.blocks.map((block) =>
       block.readings.slice(0, block.count).reduce((acc, item) => {
         const { gx, gy, gz } = item;
@@ -72,10 +98,9 @@ readFile(src, function (err, data) {
     ext: '.json'
   });
 
-  console.log(`Writing to ${resPath}`);
-
   writeFile(resPath, JSON.stringify(out), function (err) {
-    console.log(`Success!`)
+    console.log(`Successfully written to ${resPath}`);
+
     if (err) {
       return console.log(err);
     }
